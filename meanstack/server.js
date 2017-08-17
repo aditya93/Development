92749784425bf725191d86ec264320d39dff1c9b
aditya93 <adityajaju93@gmail.com>
2017-08-17 18:18:23 +0530

    Username and Email are unique

--- meanstack/server.js
+++ meanstack/server.js
@@ -3,13 +3,15 @@ var app = express();
 var port = process.env.PORT || 8080;
 var morgan = require("morgan");
 var mongoose = require('mongoose');
-var User = require('./app/models/user');
 var TestData = require('./app/models/user');
 var bodyParser = require('body-parser');
+var router = express.Router();
+var appRoutes = require('./app/routes/api')(router);
 
+app.use(morgan('dev'));
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(morgan('dev'));
+app.use('/api',appRoutes);
 
 mongoose.connect('mongodb://localhost:27017/tutorial',function(err){
 	if(err)
@@ -22,27 +24,11 @@ mongoose.connect('mongodb://localhost:27017/tutorial',function(err){
 	}
 });
 
-app.post('/users',function(req,res){
-	var user = new User();
-	user.username = req.body.username;
-	user.password = req.body.password;
-	user.email = req.body.email;
-	user.save(function(err){
-		if(err){
-			res.send(err);
-		}
-		else{
-			res.send("User Created!!");
-		}
-	});
-
-	
-});
-
 app.get('/', function (req, res) {
   res.send('Hello World!');
 })
 
 app.listen(port, function () {
   console.log('Example app listening on port '+ port);
-})
+});
+
