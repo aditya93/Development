27f0e1510f8c57fe826ab8bed28499414e751106
aditya93 <adityajaju93@gmail.com>
2017-10-06 18:14:33 +0530

    Routes restricted

--- meanstack/public/app/routes.js
+++ meanstack/public/app/routes.js
@@ -11,51 +11,57 @@ var app=angular.module("appRoutes",['ngRoute'])
 		.when('/register',{
 	templateUrl:'app/views/pages/users/register.html',
 	controller:'regCtrl',
-	controllerAs:'register'
-	//authenticated:false
+	controllerAs:'register',
+	authenticated:false
 		})
 		.when('/login',{
-	templateUrl:'app/views/pages/users/login.html'
-	//authenticated:false
+	templateUrl:'app/views/pages/users/login.html',
+	authenticated:false
 
 		})
 		.when('/logout',{
-	templateUrl:'app/views/pages/users/logout.html'
-	//authenticated:true
+	templateUrl:'app/views/pages/users/logout.html',
+	authenticated:true
 		})
 		.when('/profile',{
-	templateUrl:'app/views/pages/users/profile.html'
-	//authenticated:true
+	templateUrl:'app/views/pages/users/profile.html',
+	authenticated:true
 		})
 		.when('/facebook/:token',{
 	templateUrl:'app/views/pages/users/social/social.html',
 	controller:'facebookCtrl',
-	controllerAs:'facebook'
+	controllerAs:'facebook',
+	authenticated:false
 		})
 		.when('/facebookerror',{
 	templateUrl:'app/views/pages/users/login.html',
 	controller:'facebookCtrl',
-	controllerAs:'facebook'
+	controllerAs:'facebook',
+	authenticated:false
 		})
 		.when('/twitter/:token',{
 	templateUrl:'app/views/pages/users/social/social.html',
 	controller:'twitterCtrl',
-	controllerAs:'twitter'
+	controllerAs:'twitter',
+	authenticated:false
 		})
 		.when('/twittererror',{
 	templateUrl:'app/views/pages/users/login.html',
 	controller:'twitterCtrl',
-	controllerAs:'twitter'
+	controllerAs:'twitter',
+	authenticated:false
 		})
 		.when('/google/:token',{
 	templateUrl:'app/views/pages/users/social/social.html',
 	controller:'googleCtrl',
-	controllerAs:'google'
+	controllerAs:'google',
+	authenticated:false
 		})
 		.when('/googleerror',{
 	templateUrl:'app/views/pages/users/login.html',
 	controller:'googleCtrl',
-	controllerAs:'google'
+	controllerAs:'google',
+	authenticated:false
 		})
 		.otherwise({ redirectTo:'/'});
  	$locationProvider.html5Mode({
@@ -64,20 +70,20 @@ var app=angular.module("appRoutes",['ngRoute'])
 	});
  });
 
-// app.run(['$rootScope','Auth','$location',function($rootScope, Auth,$location){
-// 	$rootScope.$on('$routeChangeStart',function(event,next,current){
-// 		if(next.$$route.authenticated==true){
-// 			if(!Auth.isLoggedIn()){
-// 				event.preventDefault();
-// 				$location.path('/');
-// 			}
-// 		}else if(next.$$route.authenticated==false){
-// 			if(Auth.isLoggedIn()){
-// 	  			event.preventDefault();
-// 				$location.path('/profile');
-// 			}
-// 		}
-// 	});
-// }]);
+ app.run(['$rootScope','Auth','$location',function($rootScope, Auth,$location){
+	$rootScope.$on('$routeChangeStart',function(event,next,current){
+		if(next.$$route.authenticated==true){
+			if(!Auth.isLoggedIn()){
+				event.preventDefault();
+				$location.path('/');
+			}
+		}else if(next.$$route.authenticated==false){
+			if(Auth.isLoggedIn()){
+	  			event.preventDefault();
+				$location.path('/profile');
+			}
+		}
+	});
+}]);  
 
 
